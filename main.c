78e89fcaf51fd4c5d4f40e1e14461a48218e0191
Sam <Samuel.Silvola@gmail.com>
2019-03-27 09:52:18 +0000

    first version

---
+++ main.c
@@ -0,0 +1,21 @@
+// this is main.c
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include "screen.h"		// for user defined header, use double quotes
+
+int main()
+{
+	int arr[80];		// just for making  a bar chart
+	srand(time(NULL));
+	for(int i=0; i<80; i++)
+		arr[i] = rand()%70 + 30;
+
+	clearScreen();
+	setColors(RED, bg(MAGENTA));
+//	printf("Printed from main.\n");
+	barChart(arr);
+	resetColors();
+//	printf("Another message.\n");
+	getchar();
+}
---
+++ screen.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include "screen.h"
+
+void barChart(int a[])
+{
+	int i, j;		// counters for my loop
+	for(i=0; i<80; i++){	// for 80 columns
+		for(j=0; j<a[i]/3; j++){
+			printf("\033[%d;%dH", 35-j, i+1);
+#ifdef UNICODE			// conditional compiling
+			printf("%s", "\u2590");
+#else
+			printf("%c", ' ');
+#endif
+		}
+	}
+}
+
+void clearScreen(void)
+{
+	printf("\033[2J");
+	fflush(stdout);		// output the escape sequence IMMEDIATETLY
+}
+
+void setColors(short fg, short bg)
+{
+	printf("\033[%d;%d;2m", fg, bg);
+	fflush(stdout);
+}
+
+void resetColors(void)
+{
+	printf("\033[0m");
+	fflush(stdout);
+}
